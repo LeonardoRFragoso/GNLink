33b9ed3573a6236f05df59811e4b7c08d727aa17
Leonardo Fragoso <leonardorfragoso@gmail.com>
2026-01-29 17:40:47 -0300

    refactor: simplificar home - manter apenas Hero, WhatWeDo e CTA

--- src/app/[locale]/page.tsx
+++ src/app/[locale]/page.tsx
@@ -1,11 +1,6 @@
 import { setRequestLocale } from 'next-intl/server';
 import HeroSection from '@/components/home/HeroSection';
-import AboutPreview from '@/components/home/AboutPreview';
 import WhatWeDoSection from '@/components/home/WhatWeDoSection';
-import ApplicationsSection from '@/components/home/ApplicationsSection';
-import TeamPreview from '@/components/home/TeamPreview';
-import ProjectsPreview from '@/components/home/ProjectsPreview';
-import NewsPreview from '@/components/home/NewsPreview';
 import CTASection from '@/components/home/CTASection';
 
 export default async function HomePage({
@@ -19,12 +14,7 @@ export default async function HomePage({
   return (
     <>
       <HeroSection />
-      <AboutPreview />
       <WhatWeDoSection />
-      <ApplicationsSection />
-      <TeamPreview />
-      <ProjectsPreview />
-      <NewsPreview />
       <CTASection />
     </>
   );
